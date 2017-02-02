9674ccbc63f1affedccd9c88b12bdaf0157896db
hrist-todorova <hrist_todorova@abv.bg>
2017-02-02 23:04:39 +0200

    Add columns to answers table
    -question_id
    -iscorrect
    -text

--- db/schema.rb
+++ db/schema.rb
@@ -6,5 +6,8 @@ ActiveRecord::Schema.define do
   end
 
   create_table :answers do |table|
+    table.column :question_id, :integer
+    table.column :iscorrect, :integer
+    table.column :text, :string
   end
 end
