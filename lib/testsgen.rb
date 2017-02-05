0b44fbe430f7e16b3077bcdfb8de789154ecb528
hrist-todorova <hrist_todorova@abv.bg>
2017-02-05 20:51:15 +0200

    remove methods to separate files

--- lib/testsgen.rb
+++ lib/testsgen.rb
@@ -1,36 +1,14 @@
 require 'rubygems'
 require 'active_record'
 require 'sqlite3'
+require 'prawn'
 
 require_relative '../config/database.rb'
-require_relative '../db/schema.rb'
+#require_relative '../config/schema.rb'
+
 require_relative '../models/question.rb'
 require_relative '../models/answer.rb'
 
-def add_question(category, question, description, answers_array = [])
-  question = Question.create(:category => "#{category}", :text => "#{question}",
-    :description => "#{description}")
-  answers_array.each { |x| add_answer(question, x[0], x[1]) }
-end
-
-def add_answer(question, text, correctness)
-  question.answers.create(:text => text, :iscorrect => correctness)
-end
-
-def delete_question_by_text(question)
-  question = Question.find_by_text(question)
-  question.destroy
-end
-
-def change_question(old_question, new_question)
-  question = Question.find_by_text(old_question)
-  question.update_attribute(:text, new_question)
-end
-
-def find_all_by_category(category)
-  Question.where(category: category)
-end
-
-def find_all_by_description(description)
-  Question.where(description: description)
-end
+require_relative './testsgen/interface.rb'
+require_relative './testsgen/pdf_class.rb'
+require_relative './testsgen/pdf_creation.rb'
--- lib/testsgen/interface.rb
+++ lib/testsgen/interface.rb
@@ -1,10 +1,10 @@
-def add_question(question, category = nil, description = nil, answer_array)
+def add_question(question, category, description, answer_array = [])
   question = Question.create(
     :text => "#{question}",
     :category => "#{category}",
     :description => "#{description}",
     )
-  answer_array.each_slice(2).to_a.each { |x| add_answer(question, x[0], x[1]) }
+  answer_array.each_slice(2).to_a.each { |x| add_answer(question, x[0], x[1].to_i) }
   question
 end
 
@@ -14,6 +14,7 @@ end
 
 def delete_question_by_text(question)
   question = Question.find_by_text(question)
+  Answer.where(question_id: question.id).delete_all
   question.destroy
 end
 
@@ -35,7 +36,7 @@ def clear_content
   Question.delete_all
 end
 
-def generate_test(questions_number, categories_hash, variants, name)
+def generate_test(questions_number, categories_hash, variants, name, options)
   #to chech if in categories we want more than we need
 
 
@@ -47,7 +48,7 @@ def generate_test(questions_number, categories_hash, variants, name)
       all.shuffle!
       final_test << all.take(number)
     end
-    make_pdf_file(final_test, "#{name}.pdf")
+    make_pdf_file(final_test, "#{name}.pdf", options)
     make_keys_pdf(final_test, "#{name}-keys.pdf")
   end
 end
