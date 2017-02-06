4ab8cc443bece49bf86a95171e6b67f669091880
hrist-todorova <hrist_todorova@abv.bg>
2017-02-06 14:33:35 +0200

    correct methods that create test and test-keys

--- lib/testsgen/pdf_creation.rb
+++ lib/testsgen/pdf_creation.rb
@@ -1,33 +1,26 @@
 def make_pdf_file(test, filename, options_hash)
   pdf = Pdf.new
-  test.each do |category_array|
-    category_array.each do |question|
-      pdf.add_question(question.text)
-      answer_char = 'a'
-      question.answers.to_a.each do |answer|
-        pdf.add_answer("#{answer_char}) " + answer.text)
-        answer_char = answer_char.succ
-      end
+  test.flatten.each do |question|
+    pdf.add_question(question.text)
+    answer_char = 'a'
+    question.answers.to_a.each do |answer|
+      pdf.add_answer("#{answer_char}) " + answer.text)
+      answer_char = answer_char.succ
     end
   end
   pdf.create_file(filename)
 end
 
 def make_keys_pdf(test, filename)
-  #not working
   pdf = Pdf.new
-  n = 1
-  test.each do |category_array|
-    category_array.each do |question|
-      all_answers = "#{n}. "
+  test.flatten.each_with_index do |question, n|
+    all_answers = "#{n}. "
       answer_char = 'a'
       question.answers.to_a.each do |answer|
         all_answers = all_answers + answer_char + ") " + answer.text + ", " if answer.iscorrect == 1
         answer_char = answer_char.succ
       end
-      pdf.add_text(all_answers[0...(all_answers.size)])
-      n += 1
-    end
+      pdf.add_text(all_answers[0...-2])
   end
   pdf.create_file(filename)
 end
