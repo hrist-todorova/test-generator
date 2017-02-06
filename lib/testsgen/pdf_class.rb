d0a5954f8577900c6a38cfdea95bdec8ef9f1557
hrist-todorova <hrist_todorova@abv.bg>
2017-02-06 20:08:10 +0200

    add more methods for pdf creation

--- lib/testsgen/pdf_class.rb
+++ lib/testsgen/pdf_class.rb
@@ -19,20 +19,31 @@ class Pdf
   end
 
   def add_date(date)
-    #to make small
     @pdf.text date, :align => :center, :style => :italic
+  end
+
+  def add_name
+    @pdf.text('Name: ' + '.' * 95)
+  end
+
+  def add_name_and_fn
+    @pdf.text('Name: ' + '.' * 95  + ' ' * 6 + 'Faculty number: ' + '.' * 20)
   end
 
   def add_text(text)
     @pdf.text text
   end
 
-  def add_question(question)
-    @pdf.text("#{@question_number}. #{question}")
+  def add_question(question, style)
+    @pdf.text("#{@question_number}. #{question}", :style => style)
     @question_number += 1
   end
 
-  def add_answer(answer)
-   @pdf.text answer, :indent_paragraphs => 10
+  def add_picture(path)
+    @pdf.image path, :width => 200, :position => :right
+  end
+
+  def add_answer(answer, style)
+   @pdf.text answer, :indent_paragraphs => 10, :style => style
   end
 end
