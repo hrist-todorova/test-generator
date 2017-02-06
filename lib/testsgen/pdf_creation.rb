5330c9109841f9fdc87b148a82e7c75a448d0353
hrist-todorova <hrist_todorova@abv.bg>
2017-02-06 21:40:49 +0200

    add helper method for keys pfds

--- lib/testsgen/pdf_creation.rb
+++ lib/testsgen/pdf_creation.rb
@@ -1,33 +1,43 @@
 def make_pdf_file(variant, test, filename, options_hash)
   pdf = Pdf.new
-  pdf.add_header(options_hash[:subject]) if(options_hash[:subject])
-  pdf.add_subheader("Variant: " + variant)
-  pdf.add_date(options_hash[:date]) if(options_hash[:date])
-  options_hash[:fn] == "yes" ? pdf.add_name_and_fn : pdf.add_name
+  pdf.add_header(options_hash[:subject]) if options_hash[:subject]
+  pdf.add_subheader('Variant: ' + variant)
+  pdf.add_date(options_hash[:date]) if options_hash[:date]
+  options_hash[:fn] == 'yes' ? pdf.add_name_and_fn : pdf.add_name
 
   test.flatten.each do |question|
-    pdf.add_question(question.text)
-    pdf.add_picture(question.picture)
-    answer_char = 'a'
-    question.answers.to_a.each do |answer|
-      pdf.add_answer("#{answer_char}) " + answer.text)
-      answer_char = answer_char.succ
-    end
+    print_questions_and_answers(pdf, question)
   end
   pdf.create_file(filename)
 end
 
+def print_questions_and_answers(pdf, question)
+  pdf.add_question(question.text, question.style)
+  pdf.add_picture(question.picture)
+  answer_char = 'a'
+  question.answers.to_a.each do |answer|
+    pdf.add_answer("#{answer_char}) " + answer.text, answer.style)
+    answer_char = answer_char.succ
+  end
+end
+
 def make_keys_pdf(variant, test, filename)
   pdf = Pdf.new
-  pdf.add_subheader("Variant: " + variant)
+  pdf.add_subheader('Variant: ' + variant)
   test.flatten.each_with_index do |question, n|
-    all_answers = "#{n}. "
-      answer_char = 'a'
-      question.answers.to_a.each do |answer|
-        all_answers = all_answers + answer_char + ") " + answer.text + ", " if answer.iscorrect == 1
-        answer_char = answer_char.succ
-      end
-      pdf.add_text(all_answers[0...-2])
+    print_correct_answers(question, n)
+    pdf.add_text(all_answers[0...-2])
   end
   pdf.create_file(filename)
+end
+
+def print_correct_answers(question, n)
+  all_answers = "#{n}. "
+  answer_char = 'a'
+  question.answers.to_a.each do |answer|
+    if answer.iscorrect == 1
+      all_answers = all_answers + answer_char + ') ' + answer.text + ', '
+    end
+    answer_char = answer_char.succ
+  end
 end
